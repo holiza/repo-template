75f850573c37bd117b6afa8bbe9d52d3ff43d3c9
Andrew Janco <apjanco@gmail.com>
2021-12-18 13:11:32 -0500

    Update convert.py

--- newlang_project/scripts/convert.py
+++ newlang_project/scripts/convert.py
@@ -11,6 +11,10 @@ from spacy.tokens import DocBin
 from spacy.util import filter_spans
 from sklearn.model_selection import train_test_split
 
+def pre_process(conllu:str):
+    """Error with _ values in pos, should be None 
+    """
+    pass
 
 def convert(export_path: str):
     export_path = Path.cwd() / export_path
