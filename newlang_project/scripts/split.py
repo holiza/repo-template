462aaf95a3afd290a59575bda8935160b37200d3
Andrew Janco <apjanco@gmail.com>
2021-12-18 10:05:33 -0500

    Create split.py

---
+++ newlang_project/scripts/split.py
@@ -0,0 +1,24 @@
+
+"""Load inception conllu data and split into train and test files"""
+import srsly
+import typer
+import warnings
+from pathlib import Path
+
+import spacy
+from spacy.tokens import DocBin
+from spacy.util import filter_spans
+from sklearn.model_selection import train_test_split
+
+
+def split(export_path: str, test_size:float, random_state:int):
+    export_path = Path.cwd() / export_path
+    assert entities_path.exists()
+    #TODO load all conllu file, make list that can be split 
+    #train_set, validation_set = train_test_split(docs, test_size=test_size)
+    #print(f'Created {len(train_set)} training docs')
+    #print(f'Created {len(validation_set)} validation docs')
+    
+
+if __name__ == "__main__":
+    typer.run(split)
