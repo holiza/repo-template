560bd2e31b76d14577d2d3c68b452b67fc7b02f4
Andrew Janco <apjanco@gmail.com>
2021-12-19 20:10:54 -0500

    Update and rename create_config.py to update_config.py

---
+++ newlang_project/scripts/update_config.py
@@ -0,0 +1,15 @@
+import typer
+from pathlib import Path
+
+import spacy
+
+
+def update_config():
+    config_path = Path('config.cfg') 
+    config_text = config_path.read_text()
+    config_text = config_text.replace('\ntrain = null\n','\ntrain = "corpus/train.spacy"\n')
+    config_text = config_text.replace('\ndev = null\n','\ndev = "corpus/dev.spacy"\n')
+    config_path.write_text(config_text)
+
+if __name__ == "__main__":
+    typer.run(update_config)
