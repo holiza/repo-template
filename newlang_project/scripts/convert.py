05012cd27b8b7c538116aca2f1bcc28c4af40d95
Andrew Janco <apjanco@gmail.com>
2021-12-21 15:17:42 -0500

    Update convert.py

--- newlang_project/scripts/convert.py
+++ newlang_project/scripts/convert.py
@@ -8,7 +8,7 @@ import subprocess
 
 import spacy
 from spacy.tokens import DocBin
-from spacy.util import filter_spans
+from spacy.util import filter_spans, get_lang_class
 from sklearn.model_selection import train_test_split
 
 
@@ -17,14 +17,19 @@ def convert(export_path: str, n_sents:int=None, lang:str):
     lang = get_lang_class(lang)
     nlp = lang()
 
-    export_path = Path.cwd() / export_path
+    export_path = Path(export_path)
     assert export_path.exists()
-    conllu_files = [f for f in export_path.iterdir()]
     
+    conllu_files = [f for f in export_path.iterdir() if f.suffix == ".conllu"]
     #convert conllu to .spacy 
     for conllu in conllu_files:
-        subprocess.run(['python', '-m', 'spacy', 'convert', f'{str(conllu)}', "./corpus", "-n 10"])
+        subprocess.run(['python', '-m', 'spacy', 'convert', f'{str(conllu)}', "./corpus/conllu", "-n 10"])
         
+    conll_files = [f for f in export_path.iterdir() if f.suffix == ".conll"]
+    #convert conll to .spacy 
+    for conll in conll_files:
+        subprocess.run(['python', '-m', 'spacy', 'convert', f'{str(conll)}', "./corpus/conll", "-n 10"])
+    #TODO identify 
     
 
 if __name__ == "__main__":
