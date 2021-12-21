dfcb160e1011891e440f05b37a017669a213229e
Andrew Janco <apjanco@gmail.com>
2021-12-21 14:55:23 -0500

    Update convert.py

--- newlang_project/scripts/convert.py
+++ newlang_project/scripts/convert.py
@@ -11,12 +11,12 @@ from spacy.tokens import DocBin
 from spacy.util import filter_spans
 from sklearn.model_selection import train_test_split
 
-def pre_process(conllu:str):
-    """Error with _ values in pos, should be None 
-    """
-    pass
 
-def convert(export_path: str):
+def convert(export_path: str, n_sents:int=None, lang:str):
+
+    lang = get_lang_class(lang)
+    nlp = lang()
+
     export_path = Path.cwd() / export_path
     assert export_path.exists()
     conllu_files = [f for f in export_path.iterdir()]
