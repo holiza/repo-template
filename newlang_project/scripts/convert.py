e52802cf69d8b1b0be00d35f783713e38504d14f
Andrew Janco <apjanco@gmail.com>
2021-12-21 15:33:04 -0500

    Update convert.py

--- newlang_project/scripts/convert.py
+++ newlang_project/scripts/convert.py
@@ -29,6 +29,10 @@ def convert(export_path: str, n_sents:int=None, lang:str):
     #convert conll to .spacy 
     for conll in conll_files:
         subprocess.run(['python', '-m', 'spacy', 'convert', f'{str(conll)}', "./corpus/conll", f"-n {n_sents}"])
+
+    conllu = [f.stem for f in conllu_files]
+    conll  = [f.stem for f in conll_files]
+    matches = [x for x in conllu if x in conll] 
     
     #TODO identify where we have both a conll and conllu file for a given text
     #For each of these, load the DocBins, merge the docs with doc.from_docs  https://spacy.io/api/doc#from_docs
