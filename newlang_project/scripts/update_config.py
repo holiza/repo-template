63d7a8e0f73fc4e7c1255853bb04d73239d59666
Andrew Janco <apjanco@gmail.com>
2021-12-22 14:30:54 -0500

    Update update_config.py

--- newlang_project/scripts/update_config.py
+++ newlang_project/scripts/update_config.py
@@ -7,8 +7,8 @@ import spacy
 def update_config():
     config_path = Path('config.cfg') 
     config_text = config_path.read_text()
-    config_text = config_text.replace('\ntrain = null\n','\ntrain = "corpus/train.spacy"\n')
-    config_text = config_text.replace('\ndev = null\n','\ndev = "corpus/dev.spacy"\n')
+    config_text = config_text.replace('\ntrain = null\n','\ntrain = "corpus/converted/train.spacy"\n')
+    config_text = config_text.replace('\ndev = null\n','\ndev = "corpus/converted/dev.spacy"\n')
     config_path.write_text(config_text)
 
 if __name__ == "__main__":
