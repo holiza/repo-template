6376b73467f98811178c2d652a9b512cbf07be11
Andrew Janco <apjanco@gmail.com>
2021-12-24 09:03:43 -0500

    Update update_config.py

--- newlang_project/scripts/update_config.py
+++ newlang_project/scripts/update_config.py
@@ -4,11 +4,13 @@ from pathlib import Path
 import spacy
 
 
-def update_config():
+def update_config(treebank:str, wanb:bool=False):
     config_path = Path('config.cfg') 
     config_text = config_path.read_text()
     config_text = config_text.replace('\ntrain = null\n','\ntrain = "corpus/converted/train.spacy"\n')
     config_text = config_text.replace('\ndev = null\n','\ndev = "corpus/converted/dev.spacy"\n')
+    if wandb:
+        config_text = config_text.replace('\n\n[training.logger]\n@loggers = "spacy.ConsoleLogger.v1"\nprogress_bar = false\n\n',f'\n\n[training.logger]\n@loggers = "spacy.WandbLogger.v2"\nproject_name = "{treebank}"\nremove_config_values = []\nlog_dataset_dir = "./assets"\nmodel_log_interval = 1000\n\n')
     config_path.write_text(config_text)
 
 if __name__ == "__main__":
